2ff7a2a37b07743247df86a03161871693759bca
m-chinmayee <mchinmayee@proton.me>
2022-11-27 11:04:13 +0530

    Update week3_module2_time_series.py
    added acf and pacf analysis

--- week3_module2_time_series.py
+++ week3_module2_time_series.py
@@ -3,7 +3,10 @@
 # In this code, data for random walk model, AR(p), MA(q) and APMA(p,q) models have been generated. Given a time series data the best model selection and order estimation has been made. Especially in case of ARMA(p, q) model, PACF is used to determine the value of 'p' and 'q'. 
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+from statsmodels.graphics.tsaplots import plot_pacf
+from statsmodels.graphics.tsaplots import plot_acf
 
 # Time series models
 def time_series_model(model, parameters):
@@ -24,7 +27,7 @@ def time_series_model(model, parameters):
     return np.cumsum(rt, axis = 0)
 
 timesteps = 1000
-instances = 10
+instances = 1
 dimension = (timesteps, instances)
 zt = np.random.normal(0, 1, dimension)
 sigmat = 0.2
@@ -32,7 +35,7 @@ sigmat = 0.2
 # Generate a random walk instance
 rw_parameters = (np.array([0, 0, 0]), np.array([0, 0]), sigmat)
 # Generate ar2
-ar2_parameters = (np.array([0, 0.4, -0.3]), np.array([0, 0]), sigmat)
+ar2_parameters = (np.array([0, 0.4, -0.3]), np.array([0.2, -0.5]), sigmat)
 time_series = time_series_model("arma22", rw_parameters)
 
 # Plot the data
@@ -40,6 +43,19 @@ plt.title("Time Series Model")
 plt.xlabel("time")
 plt.ylabel("returns")
 plt.plot(np.arange(timesteps), time_series)
+
+# Next step is to evaluate the auto-correlation and PACF for the generated data. ACF basically gives the correlation between a time-lagged value (say, between t and t+h) where the intermediate values ocurring during the interval 'h' is ignored. PACF evaluates a conditional correlation such that the intermediate values which may have an effect on the correlation between the extreme points of 'h' interval are taken care of. 
+# Note that for the plot_acf/plot_pacf functions, the argument has to be a 1D array i.e., instances should be initiated to 1.
+plot_acf(time_series)
+plot_pacf(time_series)
 plt.show()
 
-# Next step is to evaluate the auto-correlation and PACF for the generated data.
+# We shall import a time series data and do the same analysis as before.
+url='https://raw.githubusercontent.com/jenfly/opsd/master/opsd_germany_daily.csv'
+df = pd.read_csv(url,sep=",")
+opsd = df['Consumption']
+plt.plot(opsd)
+plot_acf(opsd)
+plot_pacf(opsd)
+plt.show()
+# From the plot it is clear that this is a particularly correlated data. 
